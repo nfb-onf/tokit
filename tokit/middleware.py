778d3930e0357597ffc0ab20470379d1ef724268
Francis lavoie <f.lavoie@onf.ca>
2011-09-23 15:53:19 -0400

    changed to use a generic name for the api_key

--- tokit/middleware.py
+++ tokit/middleware.py
@@ -17,8 +17,8 @@ def get_api_key(aKey) :
         return None
 
 def extract_api_key(request):
-    if request.META.get("ONFB_API_KEY") :
-        return request.META.get("ONFB_API_KEY")
+    if request.META.get("API_KEY") :
+        return request.META.get("API_KEY")
     method = getattr(request, request.method)
     if method.get("api_key") :
         return method.get("api_key")
